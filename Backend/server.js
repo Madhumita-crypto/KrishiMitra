d433eca9e4eec81eb2b890bea34e5384a62beffe
Piyushk2428 <144818366+Piyushk2428@users.noreply.github.com>
2025-04-06 11:37:14 +0530

    Add files via upload

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+app.use('/api/auth', authRoutes);
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
